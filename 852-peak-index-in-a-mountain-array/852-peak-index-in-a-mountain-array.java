4e0442cdafd161985bfe7c34055f60b4dbdc07ca
Asgar <72298542+asgarsidd@users.noreply.github.com>
2022-09-22 10:24:45 +0530

    Time: 2 ms (12.54%), Space: 73.1 MB (48.67%) - LeetHub

---
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        for(int i = 1; i<arr.length; i++){
+            if(arr[i-1] > arr[i]){
+                return i-1;
+            }
+             // 
+        }
+      return arr.length-1;
+    }
+}
+ 
